376951d3fbd633ccefac6f4703d9d4cfb0af6c19
ram <rubiconvale@gmail.com>
2014-05-21 18:12:35 -0700

    Added a controller

--- src/main/java/com/mvc/InputController.java
+++ src/main/java/com/mvc/InputController.java
@@ -1,8 +1,13 @@
 package com.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 5/21/2014.
  * Copyright Vale Inc.
  */
+@Controller
+@RequestMapping(value = "/")
 public class InputController {
 }
