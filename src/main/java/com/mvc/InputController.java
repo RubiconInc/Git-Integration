db52e22eaf3a29eb05d3651994da0f709c29aacb
ram <rubiconvale@gmail.com>
2014-05-22 08:37:28 -0700

    Updated with log and controller

--- src/main/java/com/mvc/InputController.java
+++ src/main/java/com/mvc/InputController.java
@@ -1,7 +1,10 @@
 package com.mvc;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Administrator on 5/21/2014.
@@ -10,4 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/")
 public class InputController {
+
+
+    Logger log= Logger.getLogger(this.getClass());
+    @RequestMapping(value = "/print", method = RequestMethod.GET)
+    public Object printMyName(){
+        return "hello";
+    }
+
 }
